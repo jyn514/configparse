ee1f3dcafca0c8ad37bbca009021f39d3dd28b85
Joshua Nelson <jyn514@gmail.com>
2019-11-28 23:10:17 -0500

    add tests

---
+++ test/test_all.py
@@ -0,0 +1,27 @@
+import json
+import sys
+from os import path
+
+import yaml
+sys.path.append(path.join(path.dirname(__file__), '..'))
+import pyautoconfig
+
+NAME = 'parsertest'
+HOME = path.expanduser('~')
+CONFIG_DIR = path.join(HOME, '.config', NAME)
+
+def write_config(ext, data):
+    with open(path.join(CONFIG_DIR, 'config.' + ext), 'w') as fd:
+        fd.write(data)
+
+def test_basic():
+    with open(path.join(HOME, '.' + NAME + '.json'), 'w') as fd:
+        fd.write(json.dumps({ "apples": "some" }))
+    write_config('yml', yaml.dump({ "bananas": "more than none"}))
+
+    p = pyautoconfig.Parser(prog=NAME)
+    p.add_argument("-a", "--apples")
+    p.add_argument("-b", "--bananas")
+    args = p.parse_args()
+    assert args.apples == 'some'
+    assert args.bananas == 'more than none'
