1f1fcba04c8e8c6fb185d310a558ded334700f65
Joshua Nelson <jyn514@gmail.com>
2019-11-28 23:53:53 -0500

    make pylint happy

--- src/core.py
+++ src/core.py
@@ -14,13 +14,13 @@ from . import backends
 
 HOME = os.path.expanduser('~')
 
-backends = tuple(filter(inspect.ismodule, vars(backends).values()))
-ext_cache = {backend: backend.get_registered_extensions()
-                         for backend in backends}
+BACKENDS = tuple(filter(inspect.ismodule, vars(backends).values()))
+EXT_CACHE = {backend: backend.get_registered_extensions()
+             for backend in BACKENDS}
 
-def files_in(dir):
-    for file in os.listdir(dir):
-        file = os.path.join(dir, file)
+def files_in(directory):
+    for file in os.listdir(directory):
+        file = os.path.join(directory, file)
         if os.path.isfile(file):
             yield file
 
@@ -53,24 +53,23 @@ def get_config_files(prog):
             yield entry
 
 
-def try_parse(file, parser, namespace, default_ext):
+def try_parse(file, namespace, default_ext):
     _, ext = os.path.splitext(file)
     if ext == '':
         ext = default_ext
-    for (backend, exts) in ext_cache.items():
+    for (backend, exts) in EXT_CACHE.items():
         if ext in exts:
             with open(file) as f:
                 config = backend.load(f)
-                return convert_to_namespace(parser, config, namespace)
+                return convert_to_namespace(config, namespace)
 
-    # TODO: allow setting a default file format?
     warnings.warn("did not find a registered backend for {}. could there be a plugin that's not installed?".format(file))
     return namespace
 
 
 class Parser(argparse.ArgumentParser):
     default_ext = ".json"
-    def __init__(self, prog=None, *args, **kwargs):
+    def __init__(self, *args, prog=None, **kwargs):
         if prog is not None:
             prog = re.sub(r'\.py$', '', prog)
         if prog is None or prog == '':
@@ -87,14 +86,14 @@ class Parser(argparse.ArgumentParser):
             namespace = argparse.Namespace()
 
         for file in get_config_files(self.prog):
-            namespace = try_parse(file, self, namespace, self.default_ext)
+            namespace = try_parse(file, namespace, self.default_ext)
 
         # override configuration with argparse's builtin parsing
         # makes CLI options take precedence over config files
         return super().parse_known_args(args, namespace)
 
 
-def convert_to_namespace(parser, config, namespace):
+def convert_to_namespace(config, namespace):
     if isinstance(config, collections.abc.Mapping):
         for key, value in config.items():
             setattr(namespace, key, value)
